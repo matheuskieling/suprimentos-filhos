7637586edb45c1a5e4a3d3a3ed9b1b5800a3e309
Matheus <matheus.kieling@hotmail.com>
2025-03-29 16:14:49 -0300

    First Commit

---
+++ src/main/java/com/suprimentos/suprimentosfilhos/SuprimentosFilhosApplication.java
@@ -0,0 +1,13 @@
+package com.suprimentos.suprimentosfilhos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuprimentosFilhosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuprimentosFilhosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suprimentos/suprimentosfilhos/SuprimentosFilhosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suprimentos.suprimentosfilhos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuprimentosFilhosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
